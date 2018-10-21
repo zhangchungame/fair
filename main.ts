42ab49720d7afb61e3d5851ad025423e62e614bd
zc <zhangchungame@163.com>
2018-10-22 00:40:30 +0800

    as

--- main.ts
+++ main.ts
@@ -1,6 +1,6 @@
 
 //% color="#D063CF"
-namespace rgb {
+namespace fairTest {
     //% block="show color $color"
     //% color.shadow="colorNumberPicker"
     export function showColor(color: number) {
