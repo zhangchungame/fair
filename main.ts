d7f4b968a4b748910d812cb834376e0306b49f80
zc <zhangchungame@163.com>
2018-10-21 23:37:59 +0800

    as

--- main.ts
+++ main.ts
@@ -1,5 +1,22 @@
-﻿namespace fair{
-    export function test(){
-        basic.showNumber(0)
+﻿/**
+ * Functions are mapped to blocks using various macros
+ * in comments starting with % (e.g., //% block).
+ * The most important macro "block" specifies that a
+ * block should be generated for a **exported** function.
+ */
+//% color="#AA278D"
+namespace basic {
+
+    //% block
+    export function helloWorld() {
+
+    }
+
+    // note that Caml casing yields lower case
+    // block text with spaces
+
+    //% block
+    export function camlCaseTwo() {
+
     }
 }
